a420074bd5dac9d719c09104dbd44bc90edb108f
Jackie Amador <jamador01@saintmarys.edu>
2017-06-08 05:56:27 -0400

    Create a dino object with a Name and ID

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = {
     addDino(ev) {
         ev.preventDefault()
         //const dinoName = ev.target.dinoName.value
-        const dino = {
+        const dino = {//this creates an object with a name property, and id property
             id: this.max + 1,
             name: ev.target.dinoName.value,
         }
@@ -16,7 +16,5 @@ const app = {
 
     },
 }
-
-
 
 app.init('#dino-form')
