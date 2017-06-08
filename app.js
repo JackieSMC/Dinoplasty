ccfcd26b116f4309b964f0f8f0e7f094655eee1b
Jackie Amador <jamador01@saintmarys.edu>
2017-06-08 05:47:34 -0400

    Fixed Dino Form setup and setup of submit handler for dino

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('yay')
+const app = {
+    init(formSelector) {
+        this.max = 0
+        document.querySelector(formSelector).addEventListener('submit', this.addDino.bind(this)) 
+    },
+
+    addDino(ev) {
+        ev.preventDefault()
+        //const dinoName = ev.target.dinoName.value
+        const dino = {
+            id: this.max + 1,
+            name: ev.target.dinoName.value,
+        }
+        console.log(dino.name, dino.id)
+        ++ this.max
+
+    },
+}
+
+
+
+app.init('#dino-form')
